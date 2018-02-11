7b3ba30a5874d2f509c17c11e02b4f91ec2f50ef
Nick Tan <nctan1002@gmail.com>
2018-02-11 02:02:43 -0500

    Added score and brick drawing

--- main.js
+++ main.js
@@ -7,11 +7,16 @@ var ctx = canvas.getContext("2d");
 //
 var paused = false;
 var pausePressed = false;
+var score = 0;
 
 // request last animation frame
 var set_fps = 60;
 var lastCalledTime;
 var fps;
+
+// controls
+var rightPressed = false;
+var leftPressed = false;
 
 // ball
 var x = canvas.width/2;
@@ -28,9 +33,26 @@ var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2;
 var paddleSpeedPerSecond = 300;
 
-// controls
-var rightPressed = false;
-var leftPressed = false;
+// bricks
+var brickRows = 3;
+var brickColumns = 6;
+var brickStartX = canvas.width/brickColumns;
+var brickX = brickStartX;
+var brickY = 40;
+var brickWidth = 50;
+var brickHeight = 20;
+var bricks = [];
+
+// set bricks
+for(var r=0; r<brickRows; r++) {
+    bricks[r] = [];
+    for(var c=0; c<brickColumns; c++) {
+        bricks[r][c] = {"x": brickX, "y": brickY};
+        brickX += brickWidth;
+    }
+    brickX = brickStartX;
+    brickY += brickHeight;
+}
 
 function drawBall() {
     ctx.beginPath();
@@ -48,11 +70,31 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+function drawText(text, x, y, text_color) {
+    ctx.font = "30px Arial";
+    ctx.fillStyle = text_color;
+    ctx.textAlign = "center";
+    ctx.fillText(text, x, y);
+}
+
+function drawBricks() {
+    ctx.beginPath();
+    for(var row=0; row<bricks.length; row++) {
+        for(var col=0; col<bricks[0].length; col++) {
+            // console.log("r:" + row + " c:" + col + " x: "+ bricks[row][col].x + " y: "+ bricks[row][col].y);
+            ctx.strokeStyle = "#0095DD";
+            ctx.strokeRect(bricks[row][col].x, bricks[row][col].y, brickWidth, brickHeight);
+        }
+    }
+    ctx.closePath();
+}
+
 function draw() {
     if(!paused) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawBall();
         drawPaddle();
+        drawBricks();
 
         dx = ballSpeedPerSecondX * delta;
         dy = ballSpeedPerSecondY * delta;
@@ -65,10 +107,11 @@ function draw() {
         if(y + dy < ballRadius) {
             dy = -dy;
             ballSpeedPerSecondY = -ballSpeedPerSecondY;
-        } else if(y + dy > canvas.height-ballRadius) { // game over detection
+        } else if(y + dy > canvas.height-ballRadius) { // paddle hit and game over detection
             if((x > paddleX) && (x < paddleX + paddleWidth)) {
                 dy = -dy;
                 ballSpeedPerSecondY = -ballSpeedPerSecondY;
+                score++;
             } else {
                 // alert("GAME OVER");
                 document.location.reload();
@@ -88,7 +131,9 @@ function draw() {
         requestAnimFrame();
     } else {
         lastCalledTime = Date.now();
+        drawText("GAME PAUSED", canvas.width/2, canvas.height/2, "red");
     }
+    document.getElementById('score').innerHTML = score;
 }
 
 function keyDownHandler(e) {
